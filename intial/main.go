9e82de3dd3aa854bd179020386abefe6b43a2393
aman <amanshitta18@gmail.com>
2024-08-14 22:39:21 +0530

    Day 14 - Following https://gobyexample.com/ 	- go generics.

--- intial/main.go
+++ intial/main.go
@@ -37,6 +37,7 @@ func main() {
 	interfaces()
 	enums()
 	struct_embedding()
+	Genrics()
 }
 
 func function_call() {
@@ -1072,5 +1073,65 @@ func struct_embedding() {
 	var d describer = co
 	fmt.Println(d)
 	fmt.Println("describer:", d.describe())
+}
+
+// Generics
+
+// TODO: Understand generics again
+
+// As an example of a generic function,
+// MapKeys takes a map of any type and returns a slice of its keys.
+// This function has two type parameters - K and V; K has the comparable constraint,
+// meaning that we can compare values of this type with the == and != operators.
+// This is required for map keys in Go.
+// V has the any constraint, meaning that it’s not restricted in any way (any is an alias for interface{}).
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func Genrics() {
+	function_call()
+
+	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+	fmt.Println("keys:", MapKeys(m))
+
+	_ = MapKeys[int, string](m)
+
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.GetAll())
 
 }
