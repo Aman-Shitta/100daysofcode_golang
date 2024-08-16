a666b68f42d1f0b058e1b28fd65666050cd783d9
aman <amanshitta18@gmail.com>
2024-08-16 21:38:27 +0530

    Day 16 - Following https://gobyexample.com/
    	- go errors

--- intial/main.go
+++ intial/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"maps"
 	"math"
@@ -38,6 +39,7 @@ func main() {
 	enums()
 	struct_embedding()
 	Genrics()
+	Errors()
 }
 
 func function_call() {
@@ -1141,5 +1143,53 @@ func Genrics() {
 	lst.Push(13)
 	lst.Push(23)
 	fmt.Println("list:", lst.GetAll())
+}
 
+// Errors
+func Errors() {
+	function_call()
+
+	f := func(arg int) (int, error) {
+		if arg == 42 {
+			return -1, errors.New("can't work with 42")
+		}
+
+		return arg + 3, nil
+	}
+
+	for _, i := range []int{7, 42} {
+
+		if val, err := f(i); err != nil {
+			fmt.Println("f failed :", err)
+		} else {
+			fmt.Println("f passed :", val)
+		}
+	}
+
+	var ErrOutOfTea = fmt.Errorf("no more tea available")
+	var ErrPower = fmt.Errorf("can't boil water")
+
+	makeTea := func(arg int) error {
+		if arg == 2 {
+			return ErrOutOfTea
+		} else if arg == 4 {
+			return fmt.Errorf("making tea : %w", ErrPower)
+		}
+		return nil
+	}
+
+	for i := range 5 {
+		if err := makeTea(i); err != nil {
+			if errors.Is(err, ErrOutOfTea) {
+				fmt.Println("We should buy new Tea!")
+			} else if errors.Is(err, ErrPower) {
+				fmt.Println("Now it is dark.")
+			} else {
+				fmt.Printf("unknown error: %s\n", err)
+			}
+			continue
+		}
+
+		fmt.Println("Tea is ready")
+	}
 }
