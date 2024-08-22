a77e1db88fe07f0a9e1ac6f432c8ddb19d47c8c1
aman <amanshitta18@gmail.com>
2024-08-22 20:44:12 +0530

    Day 18 - Following https://gobyexample.com/ 	- go channels bufferting 	- go channel synchronization 	- go directions 	- go channel (https://go.dev/tour/concurrency/1)

--- intial/main.go
+++ intial/main.go
@@ -43,6 +43,10 @@ func main() {
 	CustomErrors()
 	Goroutines()
 	Channels()
+	bufferedChannel()
+	channelSync()
+	channelDirections()
+	ChannelUnderstand()
 }
 
 func function_call() {
@@ -1237,7 +1241,7 @@ func CustomErrors() {
 //  Goroutines
 
 func f(from string) {
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 2; i++ {
 		fmt.Printf("%s : %d\n", from, i)
 	}
 }
@@ -1258,7 +1262,7 @@ func Goroutines() {
 
 }
 
-// Channels
+// Channels - Follow for more bried on concurrency with goroutunes and channels (https://go.dev/tour/concurrency/1)
 
 // Channels are the pipes that connect concurrent goroutines.
 // You can send values into channels from one goroutine and receive those values into another goroutine.
@@ -1284,4 +1288,105 @@ func Channels() {
 	// This property allowed us to wait at the end
 	// of our program for the "Hello" message without having to use any other synchronization
 
+}
+
+// Buffered Channels
+// By default channels are unbuffered,
+// meaning that they will only accept sends (chan <-)
+// if there is a corresponding receive (<- chan) ready to receive the sent value.
+// Buffered channels accept a limited number of values without a corresponding receiver for those values.
+
+func bufferedChannel() {
+	function_call()
+
+	// Here we make a channel of strings buffering up to 2 values.
+	messages := make(chan string, 2)
+
+	// Because this channel is buffered,
+	// we can send these values into the channel without a corresponding concurrent receive.
+	messages <- "Buffered"
+	messages <- "channel"
+
+	// Later we can receive these two values as usual.
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+}
+
+// We can use channels to synchronize execution across goroutines.
+// Here’s an example of using a blocking receive to wait for a goroutine to finish.
+// When waiting for multiple goroutines to finish, you may prefer to use a WaitGroup(https://gobyexample.com/waitgroups).
+func channelSync() {
+	function_call()
+
+	// This is the function we’ll run in a goroutine.
+	// The done channel will be used to notify another goroutine that this function’s work is done.
+	worker := func(done chan bool) {
+		fmt.Print("working...")
+		time.Sleep(time.Second)
+		fmt.Println("done")
+
+		// Send a value to notify that we’re done.
+		done <- true
+	}
+
+	// Start a worker goroutine, giving it the channel to notify on.
+	done := make(chan bool, 1)
+
+	go worker(done)
+
+	// Block until we receive a notification from the worker on the channel.
+	<-done
+
+	// If you removed the <- done line from this program, the program would exit before the worker even started.
+}
+
+// Channel Directions
+
+func channelDirections() {
+	function_call()
+
+	// When using channels as function parameters,
+	// you can specify if a channel is meant to only send or receive values.
+	// This specificity increases the type-safety of the program.
+
+	// This ping function only accepts a channel for sending values.
+	// It would be a compile-time error to try to receive on this channel.
+	ping := func(pings chan<- string, msg string) {
+		pings <- msg
+	}
+
+	// The pong function accepts one channel for receives (pings) and a second for sends (pongs).
+	pong := func(pings <-chan string, pongs chan<- string) {
+		msg := <-pings
+		pongs <- msg
+	}
+
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func ChannelUnderstand() {
+	function_call()
+
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
 }
